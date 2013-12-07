f48fb12ed0a053972fdfadd9bbc65e2dc8c8ebc7
Kevin Hsieh <kevinshsieh@gmail.com>
2013-12-07 00:00:00 -0500

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foster2adopt::Application.config.secret_key_base = 'cb295f2f85b37db4c382c037f449ec7931502d1e8ed0a5c1296ab20aca16b57287e60ab96bd20ad902f2f7dd45e640f31120c3bff00cfdb7bb04b2dabef8640d'
